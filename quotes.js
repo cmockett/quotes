619c0962ed0c8c9a95dc549feb25a1321437cc9b
Student <student@RU-109.local>
2015-09-25 11:21:54 -0600

    5th

--- quotes.js
+++ quotes.js
@@ -7,6 +7,7 @@ var quotesfunction = function($scope){
 		var quoteObject = {}
 		quoteObject.quote = $scope.quoteToAdd || "empty"
 		quoteObject.author = $scope.authorToAdd || "empty"
+		quoteObject.rating = $scope.ratingToAdd
 		$scope.quoteArray.push(quoteObject)
 		$scope.quote = ""
 		$scope.author = ""
@@ -18,19 +19,21 @@ var quotesfunction = function($scope){
 
 		// quoteObject.rating = $scope.ratingToAdd 
 		$scope.submitRating = function($index){
-			quoteObject.rating = $scope.ratingToAdd
+			// quoteObject.rating = $scope.ratingToAdd
 			// $scope.quoteArray.sort(quoteObject.rating)
+			
+
 			console.log($scope.quoteArray)
-		}
-
-
-		$scope.ratingToAdd = quoteObject.rating
 		// $scope.rating0 = quoteObject.rating = 0
 		// $scope.rating1 = quoteObject.rating = 1
 		// $scope.rating2 = quoteObject.rating = 2
 		// $scope.rating3 = quoteObject.rating = 3
 		// $scope.rating4 = quoteObject.rating = 4
 		// $scope.rating5 = quoteObject.rating = 5
+		}
+
+
+		// $scope.ratingToAdd = quoteObject.rating
 	}
 
 			
